3de174d0bf1c4fbcb9ca948b619e793ea8ecb9b8
Chandni Kumari <ck623324@gmail.com>
2025-12-18 11:27:15 +0530

    Backend: Implemented UserProfile model and GET/POST APIs

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import health_check
+from .views import health_check, user_list_create
 urlpatterns=[
     path('health/',health_check),
+    path('users/',user_list_create)
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# Create your views here.
+from rest_framework import status
+
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+
+
 @api_view(['GET'])
 def health_check(request):
     return Response({
         "status": "Success",
         "message": "Django REST API is working"
     })
+
+@api_view(['GET', 'POST'])
+def user_list_create(request):
+
+    # GET → Fetch all users
+    if request.method == 'GET':
+        users = UserProfile.objects.all()
+        serializer = UserProfileSerializer(users, many=True)
+        return Response(serializer.data)
+
+    # POST → Create new user
+    if request.method == 'POST':
+        serializer = UserProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
