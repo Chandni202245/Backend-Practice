1c3a29b025af60801193fb81d733e794e1801671
Chandni Kumari <ck623324@gmail.com>
2025-12-19 22:37:28 +0530

    Complete CRUD REST API with GET, POST, PUT, PATCH, DELETE

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import health_check, user_list_create
+from .views import health_check, user_list_create, user_detail
 urlpatterns=[
     path('health/',health_check),
-    path('users/',user_list_create)
+    path('users/',user_list_create),
+    path('users/<int:pk>/',user_detail),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -4,8 +4,6 @@ from rest_framework import status
 
 from .models import UserProfile
 from .serializers import UserProfileSerializer
-
-
 @api_view(['GET'])
 def health_check(request):
     return Response({
@@ -34,4 +32,46 @@ def user_list_create(request):
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+from django.shortcuts import get_object_or_404
+
+@api_view(['GET','PUT','PATCH','DELETE'])
+def user_detail(request,pk):
+    user=get_object_or_404(UserProfile,pk=pk)
+
+    #GET -> Fetch single user
+    if request.method=='GET':
+        serializer=UserProfileSerializer(user)
+        return Response(serializer.data)
+
+    # PUT-> Full update
+    if request.method == 'PUT':
+        serializer=UserProfileSerializer(user,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    # PATCH -> Partial update
+    if request.method=='PATCH':
+        serializer=UserProfileSerializer(
+            user,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    # DELETE -> delete user
+    if request.method=='DELETE':
+        user.delete()
+        return Response(
+            {
+                "message": "User deleted successfully"
+            },
+            status=status.HTTP_204_NO_CONTENT
         )
